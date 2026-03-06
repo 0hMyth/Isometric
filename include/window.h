393481b51e8453d46b0b99f4ddf0656df2cfd81e
0hMyth <sashko5115@gmail.com>
2026-03-06 13:33:11 +0100

    making something

--- include/window.h
+++ include/window.h
@@ -3,3 +3,19 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+class OpenWindow {
+private:
+    GLFWwindow* window;
+    int width;
+    int height;
+    std::string title;
+
+public:
+    OpenWindow(int w, int h, const std::string& t);
+    ~OpenWindow();
+
+    bool shouldClose() const;
+    void clear(float r, float g, float b, float a);
+    void display();
+    void maximize();
+};
---
+++ src/window.cpp
@@ -0,0 +1,44 @@
+#include "include/window.h"
+#include <iostream>
+
+OpenWindow::OpenWindow(int w, int  h, const std::string& t) : width(w), height(h), title(t) {
+    if (!glfwInit()) {
+        exit(-1);
+    }
+
+    window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        exit(-1);
+    }
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+}
+
+OpenWindow::~OpenWindow() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+bool OpenWindow::shouldClose() const {
+    return glfwWindowShouldClose(window);
+}
+
+void OpenWindow::clear(float r, float g, float b, float a) {
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void OpenWindow::display() {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+void OpenWindow::maximize() {
+    glfwMaximizeWindow(window);
+}
+
+
+
